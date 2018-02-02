83a1c63bb59424731dfcf04590433946a7e0819f
Joseph Anthony Castillon <AnthonyCastillon@Josephs-MacBook-Pro.local>
2018-02-02 18:44:07 -0500

    Code to delete items from MongoDB

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,41 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+
+  console.log('Connected to MongoDB server');
+
+  // deleteMany
+  // db.collection('Todos').deleteMany({text: 'Buy groceries'}).then((result) =>{
+  //   console.log(result);
+  // });
+
+  // deleteOne
+// db.collection('Todos').deleteOne({text: 'Buy groceries'}).then((result) => {
+//   console.log(result);
+// });
+
+
+  // findOneAndDelete
+// db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+//   console.log(result);
+// });
+
+
+  // db.collection('Users').deleteMany({name: 'Sam'}).then((result) => {
+  //   console.log(`After deleting many:  ${result}`);
+  // });
+
+db.collection('Users').findOneAndDelete(
+  {
+    _id: new ObjectID('5a6fd588524f3b226327dec2')
+  }).then((res) => {
+    console.log(JSON.stringify(res, undefined, 2));
+});
+
+
+  // db.close();
+});
--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -8,16 +8,18 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 
   console.log('Connected to MongoDB server');
 
-  // db.collection('Todos').find({
-  //     // completed: false
-  //     // _id: '5a73cd1bac30a532410e6ea1'  // does not work
-  //     _id: new ObjectID('5a73cd1bac30a532410e6ea1')
-  //   }).toArray().then((docs) => {
-  //   console.log('Todos');
-  //   console.log(JSON.stringify(docs, undefined, 2));
-  // }, (err) => {
-  //   console.log('Unable to get todos from the database');
-  // });
+  db.collection('Todos').find(
+    // {
+      // completed: false
+      // _id: '5a73cd1bac30a532410e6ea1'  // does not work
+    //   _id: new ObjectID('5a73cd1bac30a532410e6ea1')
+    // }
+  ).toArray().then((docs) => {
+    console.log('Todos');
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log('Unable to get todos from the database');
+  });
 
   // db.collection('Todos').find().count().then((count) => {
   //   console.log(`Todos count:  ${count}`);
@@ -26,12 +28,14 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   // });
 
 
-  db.collection('Users').find({name: 'Anthony'}).toArray().then((docs)=>{
-    console.log('Users');
-    console.log(JSON.stringify(docs, undefined, 2))
-  }, (err) =>{
-    console.log('Unable to access Users data')
-  });
+  // db.collection('Users').find({
+  //   name: 'Anthony'
+  // }).toArray().then((docs)=>{
+  //   console.log('Users');
+  //   console.log(JSON.stringify(docs, undefined, 2))
+  // }, (err) =>{
+  //   console.log('Unable to access Users data')
+  // });
 
 
   // db.close();
