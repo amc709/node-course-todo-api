a5bebbd043c61f8a845ebf79f2ae145635f894df
Joseph Anthony Castillon <AnthonyCastillon@Josephs-MacBook-Pro.local>
2018-02-14 18:35:34 -0500

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -65,6 +65,17 @@ UserSchema.methods.generateAuthToken = function(){
   });
 };
 
+
+UserSchema.methods.removeToken = function(token){
+  var user = this;
+  return user.update({
+    $pull: {
+      tokens: {token}
+    }
+  })
+};
+
+
 // statics creates model methods (not instance methods)
 UserSchema.statics.findByToken = function(token){
   var User = this;
--- server/server.js
+++ server/server.js
@@ -188,9 +188,19 @@ app.post('/users/login', (req, res) => {
       res.header('x-auth', token).send(user);
     });
   }). catch((e) => {
-    res.status(400).send(e);
+    res.status(400).send();
   });
 
+});
+
+
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  });
 });
 
 
